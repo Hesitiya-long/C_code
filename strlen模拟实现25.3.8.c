d50fa729395b3da518445bf0236a2ae6f8dabbe4
Hesitiya-long <2037589450@qq.com>
2025-03-09 22:39:08 +0800

    Add files via upload
    strlen模拟实现25.3.8

---
+++ strlen模拟实现25.3.8.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<assert.h>
+
+//计算器方式计数
+size_t  my_strlen1(const char* str)
+{
+	assert(str);
+	size_t count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+//指针相减方式计数
+size_t  my_strlen2(const char* str)
+{
+	assert(str);
+	char* str2 = str;
+	while (1)
+	{
+		if (*str == '\0')
+		{
+			break;
+		}
+		str++;
+	}
+	return (size_t)(str - str2);
+}
+
+//递归方式计数
+size_t  my_strlen3(const char* str)
+{
+	if (*str != '\0')
+		return my_strlen3(str + 1) + 1;
+	else
+		return 0;
+
+}
+
+int main()
+{
+	char arr[] = "abcdefgfajskngladgji";
+
+	size_t num1 = my_strlen1(arr);
+	size_t num2 = my_strlen2(arr);
+	size_t num3 = my_strlen3(arr);
+
+	printf("%zu\n", num1);
+	printf("%zu\n", num2);
+	printf("%zu\n", num3);
+
+	return 0;
+}
+
+
