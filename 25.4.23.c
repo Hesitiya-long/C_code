75b560bd52f65ea92d99b7abffce34987bc71f8b
Hesitiya-long <2037589450@qq.com>
2025-04-23 22:42:03 +0800

    Add files via upload
    25.4.23

---
+++ 25.4.23.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+int main()
+{
+    int i, num;
+    num = 2;
+    for (i = 0;i < 3;i++)
+    {
+        printf("num 变量为 %d \n", num);
+        num++;
+        {
+            auto int num = 1;
+            printf("内置模块 num 变量为 %d \n", num);
+            num++;
+        }
+    }
+    return 0;
+}
