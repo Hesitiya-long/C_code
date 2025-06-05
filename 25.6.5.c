704ee2d7f2b611ab4d1270d9fc7bfbb362030721
Hesitiya-long <2037589450@qq.com>
2025-06-05 20:55:37 +0800

    Add files via upload
    25.6.5

---
+++ 25.6.5.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//  Created by www.runoob.com on 15/11/9.
+//  Copyright © 2015年 菜鸟教程. All rights reserved.
+//
+
+#include <stdio.h>
+
+// 计算阶乘的函数
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    unsigned long long sum = 0;
+
+    // 计算1 + 2! + 3! + ... + 20!
+    for (int i = 1; i <= 20; i++) {
+        sum += factorial(i);
+    }
+
+    // 输出结果
+    printf("1 + 2! + 3! + ... + 20!的和是: %llu\n", sum);
+
+    return 0;
+}
